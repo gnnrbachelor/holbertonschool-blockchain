44408cdf74b5a14bfbba1f533fe30ea33dd40cd2
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-18 22:23:12 +0000

    Add sig verification

---
+++ crypto/ec_verify.c
@@ -0,0 +1,22 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_verity - Verifies sig
+ * @key: Key
+ * @msg: Message
+ * @msglen: Length
+ * @sig: Signature
+ * Return: Sig or NULL
+ */
+
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
+{
+	int res = 0;
+
+	if (!key || !msg || !sig)
+		return (res);
+
+	res = ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key);
+	return (res == 1);
+}
+

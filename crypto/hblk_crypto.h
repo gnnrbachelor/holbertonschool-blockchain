391627b9ea11be832a15eb55c6b400ca17e6dcfa
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-18 09:53:40 +0000

    Fixing betty

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -21,7 +21,8 @@
 
 
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 
 #endif

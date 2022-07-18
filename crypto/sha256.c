706fcb39caddc2f7bc0493426bcb5dae9e5d6d0f
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-18 09:50:07 +0000

    Fix betty

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -9,7 +9,8 @@
  *
  */
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
 		return (NULL);

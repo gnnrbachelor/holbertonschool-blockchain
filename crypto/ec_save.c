7cd1c436548e7307512918935a2e32588dc50e5a
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-19 11:46:20 +0000

    Refactor ec_save for debugging

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,5 +1,56 @@
 #include "hblk_crypto.h"
 
+static int priv_save(EC_KEY *key, char const *folder)
+{
+
+	FILE *priv_file = NULL;
+	char *priv_path = NULL;
+
+	priv_path = calloc(strlen(folder) + strlen(PRIVATE) + 1, 1);
+	if (!priv_path)
+		return (0);
+
+	strcpy(priv_path, folder);
+	strcat(priv_path, PRIVATE);
+	priv_file = fopen(priv_path, "w+");
+	if (!PEM_write_ECPrivateKey(priv_file, key, NULL, NULL, 0, NULL, NULL))
+	{
+		fclose(priv_file);
+		free(priv_path);
+		return (0);
+	}
+	fclose(priv_file);
+	free(priv_path);
+	return (1);
+
+}
+
+static int pub_save(EC_KEY *key, char const *folder)
+{
+
+	FILE *pub_file = NULL;
+	char *pub_path = NULL;
+
+	pub_path = calloc(strlen(folder) + strlen(PUBLIC) + 1, 1);
+	if (!pub_path)
+		return (0);
+
+	strcpy(pub_path, folder);
+	strcat(pub_path, PUBLIC);
+	pub_file = fopen(pub_path, "w+");
+	if (!PEM_write_EC_PUBKEY(pub_file, key))
+	{
+		fclose(pub_file);
+		free(pub_path);
+		return (0);
+	}
+	fclose(pub_file);
+	free(pub_path);
+
+	return (1);
+
+}
+
 
 /**
  * ec_save - Saves EC key pair
@@ -11,45 +62,18 @@
 
 int ec_save(EC_KEY *key, char const *folder)
 {
-	FILE *priv_file, *pub_file;
-	char *priv_path = NULL, *pub_path = NULL;
-
 
 	if (!key || !folder)
 		return (0);
 
 	mkdir(folder, 0777);
 
-	priv_path = calloc(strlen(folder) + strlen(PRIVATE) + 1, 1);
-	if (!priv_path)
+	if (!pub_save(key, folder))
 		return (0);
 
-	pub_path = calloc(strlen(folder) + strlen(PUBLIC) + 1, 1);
-	if (!pub_path)
-	{
-		free(priv_path);
+	if (!priv_save(key, folder))
 		return (0);
-	}
 
-	strcpy(priv_path, folder);
-	strcat(priv_path, PRIVATE);
-	priv_file = fopen(priv_path, "w+");
-	if (!PEM_write_ECPrivateKey(priv_file, key, NULL, NULL, 0, NULL, NULL))
-	{
-		fclose(priv_file);
-		return (0);
-	}
-	fclose(priv_file);
-
-	strcpy(pub_path, folder);
-	strcat(pub_path, PUBLIC);
-	pub_file = fopen(pub_path, "w+");
-	if (!PEM_write_EC_PUBKEY(pub_file, key))
-	{
-		fclose(pub_file);
-		return (0);
-	}
-	fclose(pub_file);
 	return (1);
 }
 
